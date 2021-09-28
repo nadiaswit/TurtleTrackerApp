5e52fe58169c788545b015192b0b413d74977fac
Nadia Swit <ns323@duke.edu>
2021-09-28 10:29:26 -0400

    Parses a pasted data line into variables

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -8,4 +8,27 @@
 # Author: Nadia Swit (nadia.swit@duje,edu)
 # Date:   Fall 2021
 #--------------------------------------------------------------
+#creating two dictionaries: one having date values and one with coordinates
+#date dictionary: key = uid, value =  date (value can be same, key diff)
+#coordinate dictionary: key = tag_id, value = lat/long coordinate pair
+#need a string method to pull out specific characters
+#treat as a delineated file
+
+#Pretend we read one line of data from the file
+lineString = '20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0'
+
+#Split the string into a list of data types
+lineData = lineString.split()
+
+#once items are parsed/split into strings, want to save as new variables
+#Extract items in list into variables
+record_id = lineData[0]
+obs_date = lineData[2]
+obs_lc = lineData[4]
+obs_lat = lineData[6]
+obs_long = lineData[7]
+
+#Print location of Sara
+#f prints entire line as string
+print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_long} on {obs_date}")
 
